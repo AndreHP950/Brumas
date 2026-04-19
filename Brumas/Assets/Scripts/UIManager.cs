499c50fbf44b6d0cd0e831290fa1a6c822bb0a50
AndreHP950 <parreirasandre950@gmail.com>
2026-04-18 22:45:25 -0300

    menu inicial com navegação básica e assets de UI
    Fiz um menu simples inicial, com três botões principais: Iniciar, Configurações e Créditos. Inicialmente apenas o Iniciar funciona, que abre um painel com a seleção de histórias. No momento temos apenas a do Benício, que redireciona para a cena Game. Fiz um script simples de UIManager Singleton para a troca de cenas. Criei assets de botões e moldura para as histórias, além de uma imagem temporária para ilustrar como planejamos o cenário do menu. Adicionei também uma fonte cartoon para os textos. Fiz a configuração do prefab do Canvas e deixei tudo responsivo. Testado em 16:9 FHD e no Simulator com o dispositivo Iphone 13 Pro Max.

---
+++ Brumas/Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
