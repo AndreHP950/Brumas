5076b8da6fc357410043da42c40d6c8d0d8b3e26
ArthurDof <arthurdoffemond@gmail.com>
2026-04-19 23:01:01 -0300

    Consertos de ultima hora
    Extendi Mais um pouco a historia agora tem mais ramificações

--- Brumas/Assets/Scripts/DialogoManager.cs
+++ Brumas/Assets/Scripts/DialogoManager.cs
@@ -11,12 +11,21 @@ public class DialogoManager : MonoBehaviour
     public int dialogoAtual;
     int chaveObtida;
     public GameObject botaoSair;
+    public GameObject botaoSairHistoria;
+    public GameObject FimBeta;
     public bool AlgoAberto;
     private void Start()
     {
         AlgoAberto = true;
         chaveObtida = 0;
         texto.text = dialog.text;
+    }
+    private void Update()
+    {
+        if (dialogoAtual < 0)
+        {
+            dialogoAtual = 0;
+        }
     }
     public void Iniciar(int dialogo)
     {
@@ -30,6 +39,12 @@ public class DialogoManager : MonoBehaviour
         texto.text = dialog.text;
         dialogoAtual++;
     }
+    public void Voltar()
+    {
+        dialog = dialog.nextDialog[1];
+        texto.text = dialog.text;
+        dialogoAtual--;
+    }
     public void Fechar()
     {
         AlgoAberto = false;
@@ -39,6 +54,20 @@ public class DialogoManager : MonoBehaviour
         if (dialogoMax == dialogoAtual)
         {
             botaoSair.SetActive(true);
+        }
+    }
+    public void ConferirHistoria(int dialogoMax)
+    {
+        if (dialogoMax == dialogoAtual)
+        {
+            botaoSairHistoria.SetActive(true);
+        }
+    }
+    public void ConferirHistoriaPlaceHolder(int dialogoMax)
+    {
+        if (dialogoMax == dialogoAtual)
+        {
+            FimBeta.SetActive(true);
         }
     }
     public void TrocarCena(string Cena)
